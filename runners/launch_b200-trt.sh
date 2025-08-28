d556b8862a6c54c6ff393fef4c7719f1e0aa6fab
Kedar Potdar <kepotdar@nvidia.com>
2025-08-28 09:58:28 -0700

    add trt init for 70b

---
+++ runners/launch_b200-trt.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+export HF_HUB_CACHE_MOUNT="/raid/hf_hub_cache/"
+export PORT_OFFSET=${USER: -1}
+
+MODEL_CODE="${1%%_*}"
+PARTITION="dgx-b200"
+SQUASH_FILE="/raid/image_${MODEL_CODE}_b200-trt.sqsh"
+
+salloc --partition=$PARTITION --gres=gpu:$TP --exclusive --time=180 --no-shell
+JOB_ID=$(squeue -u $USER -h -o %A)
+
+set -x
+srun --jobid=$JOB_ID bash -c "enroot import -o $SQUASH_FILE docker://$IMAGE"
+srun --jobid=$JOB_ID \
+--container-image=$SQUASH_FILE \
+--container-mounts=$GITHUB_WORKSPACE:/workspace/,$HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
+--container-mount-home \
+--container-workdir=/workspace/ \
+--no-container-entrypoint --export=ALL \
+bash benchmarks/${MODEL_CODE}_b200-trt_slurm.sh
+
+scancel $JOB_ID
