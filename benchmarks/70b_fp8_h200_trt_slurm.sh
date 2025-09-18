a267b4bd52f1cd162813fedfdcffb73936128dab
taoli <litaotju@users.noreply.github.com>
2025-09-18 14:54:39 +0800

    Update max_num_tokens, shall be large enough to schedule multiple ctx in one batch

--- benchmarks/70b_fp8_h200_trt_slurm.sh
+++ benchmarks/70b_fp8_h200_trt_slurm.sh
@@ -46,7 +46,7 @@ stream_interval: 10
 EOF
 fi
 
-mpirun -n 1 --oversubscribe --allow-run-as-root trtllm-serve $MODEL --tp_size $TP --trust_remote_code --max_seq_len $MAX_MODEL_LEN --max_num_tokens $MAX_MODEL_LEN --num_postprocess_workers 2 --extra_llm_api_options llama-config.yml --port $PORT > $SERVER_LOG 2>&1 &
+mpirun -n 1 --oversubscribe --allow-run-as-root trtllm-serve $MODEL --tp_size $TP --trust_remote_code --max_seq_len $MAX_MODEL_LEN --max_num_tokens 16384 --num_postprocess_workers 2 --extra_llm_api_options llama-config.yml --port $PORT > $SERVER_LOG 2>&1 &
 
 set +x
 while IFS= read -r line; do
