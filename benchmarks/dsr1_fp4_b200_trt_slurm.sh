07d82b1efb9d52d2f36e84bd3698eaee63c2667e
Kedar Potdar <kepotdar@nvidia.com>
2025-09-17 18:13:31 -0700

    add model download step

--- benchmarks/dsr1_fp4_b200_trt_slurm.sh
+++ benchmarks/dsr1_fp4_b200_trt_slurm.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
-# ========= Required Env Vars =========
+# === Required Env Vars === 
 # HF_TOKEN
 # HF_HUB_CACHE
+# IMAGE
 # MODEL
-# PORT
+# ISL
+# OSL
+# MAX_MODEL_LEN
+# RANDOM_RANGE_RATIO
 # TP
 # CONC
-# MAX_MODEL_LEN
+# RESULT_FILENAME
+# PORT_OFFSET
 
-echo "JOB $SLURM_JOB_ID running on NODE $SLURMD_NODENAME"
+echo "JOB $SLURM_JOB_ID running on $SLURMD_NODENAME"
 
 echo "TP: $TP, CONC: $CONC, ISL: $ISL, OSL: $OSL"
+
+hf download $MODEL
 
 # ========= Determine EP_SIZE and MOE_BACKEND based on ISL, OSL, CONC =========
 EP_SIZE=""
@@ -70,6 +77,8 @@ moe_config:
 EOF
 
 set -x
+
+# Launch TRT-LLM server
 if [[ -n "$EP_SIZE" ]]; then
     mpirun -n 1 --oversubscribe --allow-run-as-root \
     trtllm-serve $MODEL --port=$PORT \
@@ -95,6 +104,12 @@ fi
 set +x
 while IFS= read -r line; do
     printf '%s\n' "$line"
+    if [[ "$line" =~ [Ee][Rr][Rr][Oo][Rr] ]]; then
+        sleep 5
+        tail -n100 $SERVER_LOG
+        echo "JOB $SLURM_JOB_ID ran on NODE $SLURMD_NODENAME"
+        exit 1
+    fi
     if [[ "$line" == *"Application startup complete"* ]]; then
         break
     fi
@@ -103,12 +118,12 @@ done < <(tail -F -n0 "$SERVER_LOG")
 git clone https://github.com/kimbochen/bench_serving.git
 set -x
 python3 bench_serving/benchmark_serving.py \
---model=$MODEL --backend=vllm \
---base-url="http://0.0.0.0:$PORT" \
---dataset-name="random" \
---random-input-len=$ISL --random-output-len=$OSL --random-range-ratio=$RANDOM_RANGE_RATIO \
---num-prompts=$(( $CONC * 10 )) --max-concurrency=$CONC \
---request-rate=inf --ignore-eos \
---save-result --percentile-metrics="ttft,tpot,itl,e2el" \
---result-dir=/workspace/ \
---result-filename=$RESULT_FILENAME.json
+--model $MODEL --backend openai \
+--base-url http://0.0.0.0:$PORT \
+--dataset-name random \
+--random-input-len $ISL --random-output-len $OSL --random-range-ratio $RANDOM_RANGE_RATIO \
+--num-prompts $(( $CONC * 10 )) --max-concurrency $CONC \
+--request-rate inf --ignore-eos \
+--save-result --percentile-metrics 'ttft,tpot,itl,e2el' \
+--result-dir /workspace/ \
+--result-filename $RESULT_FILENAME.json
