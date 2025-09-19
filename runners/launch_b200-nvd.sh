f87a97bb34763a89f6e94a2189b414138f332cc2
Kedar Potdar <kepotdar@nvidia.com>
2025-09-19 11:13:26 -0700

    slightly modify image name logic for trt on nvd docker runners

--- runners/launch_b200-nvd.sh
+++ runners/launch_b200-nvd.sh
@@ -18,7 +18,7 @@ docker run --rm -d --network $network_name --name $server_name \
 -e HF_TOKEN -e HF_HUB_CACHE -e MODEL -e TP -e CONC -e MAX_MODEL_LEN -e PORT=$PORT \
 -e TORCH_CUDA_ARCH_LIST="10.0" -e CUDA_DEVICE_ORDER=PCI_BUS_ID -e CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
 --entrypoint=/bin/bash \
-${IMAGE/\#/\/} \
+$(echo "$IMAGE" | sed 's/#/\//') \
 benchmarks/"${EXP_NAME%%_*}_${PRECISION}_b200${FRAMEWORK_SUFFIX}_docker.sh"
 
 set +x
@@ -36,7 +36,7 @@ docker run --rm --network $network_name --name $client_name \
 -v $GITHUB_WORKSPACE:/workspace/ -w /workspace/ \
 -e HF_TOKEN -e PYTHONPYCACHEPREFIX=/tmp/pycache/ \
 --entrypoint=/bin/bash \
-${IMAGE/\#/\/} \
+$(echo "$IMAGE" | sed 's/#/\//') \
 -lc "pip install -q datasets pandas && \
 python3 bench_serving/benchmark_serving.py \
 --model $MODEL  --backend vllm --base-url http://$server_name:$PORT \
