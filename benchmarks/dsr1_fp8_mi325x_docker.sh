bbb11481974e4afd1ff430c65bc0b3cf7931fc8f
vbayanag <147658931+vbayanag@users.noreply.github.com>
2025-09-19 13:33:15 -0700

    Update dsr1_fp8_mi325x_docker.sh

--- benchmarks/dsr1_fp8_mi325x_docker.sh
+++ benchmarks/dsr1_fp8_mi325x_docker.sh
@@ -10,16 +10,20 @@
 # MAX_MODEL_LEN
 
 # Reference
-# https://rocm.docs.amd.com/en/docs-7.0-rc1/preview/benchmark-docker/inference-sglang-deepseek-r1-fp8.html#run-the-inference-benchmark
+# https://rocm.docs.amd.com/en/docs-7.0-docker/benchmark-docker/inference-sglang-deepseek-r1-fp8.html
 
 export SGLANG_USE_AITER=1
-set -x
+
+USE_FASTSAFETENSOR=1 \
+SAFETENSORS_FAST_GPU=1 \
 python3 -m sglang.launch_server \
---model-path=$MODEL --host=0.0.0.0 --port=$PORT --trust-remote-code \
---tensor-parallel-size=$TP \
---mem-fraction-static=0.8 \
---cuda-graph-max-bs=$CONC \
---chunked-prefill-size=196608 \
---num-continuous-decode-steps=4 \
---max-prefill-tokens=196608 \
---disable-radix-cache
+    --model-path $MODEL \
+    --port $PORT \
+    --tensor-parallel-size $TP \
+    --trust-remote-code \
+    --chunked-prefill-size 196608 \
+    --mem-fraction-static 0.8 --disable-radix-cache \
+    --num-continuous-decode-steps 4 \
+    --max-prefill-tokens 196608 \
+    --cuda-graph-max-bs 128
+    
