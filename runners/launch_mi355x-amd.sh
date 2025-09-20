b2164ced783f0595819d69320cc89e9955f87f37
Kimbo Chen <kimbo@semianalysis.com>
2025-09-20 15:56:08 +0000

    Test changing to docker.

--- runners/launch_mi355x-amd.sh
+++ runners/launch_mi355x-amd.sh
@@ -14,18 +14,56 @@
 # RESULT_FILENAME
 # HF_TOKEN
 
-MODEL_CODE="${EXP_NAME%%_*}"
+sudo sh -c 'echo 0 > /proc/sys/kernel/numa_balancing'
+
 HF_HUB_CACHE_MOUNT="/nfsdata/hf_hub_cache-${USER: -1}/"
+PORT=8888
+
+network_name="bmk-net"
+server_name="bmk-server"
+client_name="bmk-client"
+
+docker network create $network_name
 
 set -x
-srun --partition=compute --exclusive \
---gres=gpu:$TP --cpus-per-task=256 --ntasks-per-node=1 --time=180 \
---container-image=$IMAGE \
---container-name="${MODEL_CODE}_container" \
---container-mounts=$GITHUB_WORKSPACE:/workspace/,$HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
---container-mount-home \
---container-writable \
---container-remap-root \
---container-workdir=/workspace/ \
---no-container-entrypoint --export=ALL,PORT=8888 \
-bash benchmarks/${MODEL_CODE}_${PRECISION}_mi355x_slurm.sh
+docker run --rm -d --ipc=host --shm-size=16g --network=$network_name --name=$server_name \
+--privileged --cap-add=CAP_SYS_ADMIN --device=/dev/kfd --device=/dev/dri --device=/dev/mem \
+--cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+-v $HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
+-v $GITHUB_WORKSPACE:/workspace/ -w /workspace/ \
+-e HF_TOKEN -e HF_HUB_CACHE -e MODEL -e TP -e CONC -e MAX_MODEL_LEN -e PORT=$PORT \
+--entrypoint=/bin/bash \
+$IMAGE \
+benchmarks/"${EXP_NAME%%_*}_${PRECISION}_mi355x_docker.sh"
+
+set +x
+while IFS= read -r line; do
+    printf '%s\n' "$line"
+    if [[ "$line" =~ Application\ startup\ complete ]]; then
+        break
+    fi
+done < <(docker logs -f --tail=0 $server_name 2>&1)
+
+git clone https://github.com/kimbochen/bench_serving.git
+
+set -x
+docker run --rm --network=$network_name --name=$client_name \
+-v $GITHUB_WORKSPACE:/workspace/ -w /workspace/ \
+-e HF_TOKEN -e PYTHONPYCACHEPREFIX=/tmp/pycache/ \
+--entrypoint=python3 \
+$IMAGE \
+bench_serving/benchmark_serving.py \
+--model=$MODEL --backend=vllm --base-url="http://$server_name:$PORT" \
+--dataset-name=random \
+--random-input-len=$ISL --random-output-len=$OSL --random-range-ratio=$RANDOM_RANGE_RATIO \
+--num-prompts=$(( $CONC * 10 )) \
+--max-concurrency=$CONC \
+--request-rate=inf --ignore-eos \
+--save-result --percentile-metrics="ttft,tpot,itl,e2el" \
+--result-dir=/workspace/ --result-filename=$RESULT_FILENAME.json
+
+while [ -n "$(docker ps -aq)" ]; do
+    docker stop $server_name
+    docker network rm $network_name
+    sleep 5
+done
