0a531aab157b30264afbc02a8da94a9da2974047
Cam Quilici <cjquilici@gmail.com>
2025-12-02 16:42:02 -0600

    updating structure of changelog

--- utils/process_changelog.py
+++ utils/process_changelog.py
@@ -3,6 +3,7 @@ import json
 import argparse
 
 from pydantic import BaseModel, ConfigDict, Field
+
 from matrix_logic.validation import load_config_files, load_runner_file
 
 MASTER_CONFIGS = [".github/configs/amd-master.yaml",
@@ -12,13 +13,9 @@ MASTER_CONFIGS = [".github/configs/amd-master.yaml",
 class ChangelogEntry(BaseModel):
     model_config = ConfigDict(extra='forbid', populate_by_name=True)
     
-    config_key: str = Field(alias='config-key')
+    config_keys: list[str] = Field(alias='config-keys')
     description: str
     seq_lens: list[str] = Field(alias='seq-lens')
-
-
-class Config(BaseModel):
-    changelog: list[ChangelogEntry]
 
 
 def main():
@@ -34,21 +31,18 @@ def main():
 
     master_config_data = load_config_files(MASTER_CONFIGS)
 
-    changelog_path = args.changelog_file
-    with open(changelog_path, 'r') as f:
+    with open(args.changelog_file, 'r') as f:
         changelog_data = yaml.safe_load(f)
+
+    for entry_data in changelog_data:
+        entry = ChangelogEntry.model_validate(entry_data)
         
-    changelog_data = Config.model_validate(changelog_data)
-    
-    # Validate all config-keys actually exist in the master config files
-    for entry in changelog_data.changelog:
-        if entry.config_key not in master_config_data.keys():
-            raise ValueError(
-                f"Changelog entry with config-key '{entry.config_key}' does not exist in master config files."
-            )
-            
-    
-         
+        # Make sure the specfied config keys actually exist in the master config files
+        for config_key in entry.config_keys:
+            if config_key not in master_config_data.keys():
+                raise ValueError(
+                    f"Config key '{config_key}' does not exist in master config files."
+                )
 
 
 if __name__ == "__main__":
