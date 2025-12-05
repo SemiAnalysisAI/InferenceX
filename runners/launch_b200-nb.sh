c33f24bd0a306eeafa61bcafdc6d88f882ffb359
Cam Quilici <cjquilici@gmail.com>
2025-12-05 13:24:43 -0600

    explicitly state IB HCA devs

--- runners/launch_b200-nb.sh
+++ runners/launch_b200-nb.sh
@@ -4,6 +4,8 @@ HF_HUB_CACHE_MOUNT="/mnt/data/hf-hub-cache/"
 PARTITION="main"
 FRAMEWORK_SUFFIX=$([[ "$FRAMEWORK" == "trt" ]] && printf '_trt' || printf '')
 
+NCCL_IB_HCA="mlx5_4,mlx5_5,mlx5_6,mlx5_7,mlx5_8,mlx5_9,mlx5_10,mlx5_11"
+
 set -x
 srun --partition=$PARTITION --gres=gpu:$TP --exclusive \
 --container-image=$IMAGE \
@@ -11,5 +13,5 @@ srun --partition=$PARTITION --gres=gpu:$TP --exclusive \
 --container-mounts=$GITHUB_WORKSPACE:/workspace/,$HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
 --no-container-mount-home --container-writable \
 --container-workdir=/workspace/ \
---no-container-entrypoint --export=ALL,PORT_OFFSET=${USER: -1} \
+--no-container-entrypoint --export=ALL,PORT_OFFSET=${USER: -1},NCCL_IB_HCA=$NCCL_IB_HCA \
 bash benchmarks/${EXP_NAME%%_*}_${PRECISION}_b200${FRAMEWORK_SUFFIX}_slurm.sh
