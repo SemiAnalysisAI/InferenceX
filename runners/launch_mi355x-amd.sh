7d90cf069324aa3f1d72dfc244c6aa090d56f3b0
seungrok.jung <seungrok.jung@amd.com>
2026-01-12 20:17:47 -0600

    docker refresh
    Signed-off-by: seungrok.jung <seungrok.jung@amd.com>

--- runners/launch_mi355x-amd.sh
+++ runners/launch_mi355x-amd.sh
@@ -150,6 +150,9 @@ else
     salloc --partition=$PARTITION --gres=gpu:$TP --cpus-per-task=256 --time=180 --no-shell
     JOB_ID=$(squeue -u $USER -h -o %A | head -n1)
 
+    if [[ "$FRAMEWORK" == "atom" ]]; then
+        srun --jobid=$JOB_ID bash -c "sudo rm $SQUASH_FILE"
+    fi
     srun --jobid=$JOB_ID bash -c "sudo enroot import -o $SQUASH_FILE docker://$IMAGE"
     srun --jobid=$JOB_ID bash -c "sudo chmod -R a+rwX /hf-hub-cache/"
     srun --jobid=$JOB_ID \
