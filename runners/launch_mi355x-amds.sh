ae1c1503a8055ad92d75d5bddcee06cf6f727171
Cam Quilici <cjquilici@gmail.com>
2026-01-20 18:14:32 -0600

    add unsquashfs check

--- runners/launch_mi355x-amds.sh
+++ runners/launch_mi355x-amds.sh
@@ -154,11 +154,11 @@ else
         srun --jobid=$JOB_ID bash -c "rm $SQUASH_FILE"
     fi
 
-    srun --jobid=$JOB_ID bash -c "sudo enroot import -o $SQUASH_FILE docker://$IMAGE"
-    if ! srun --jobid=$JOB_ID bash -c "sudo unsquashfs -l $SQUASH_FILE > /dev/null"; then
+    srun --jobid=$JOB_ID bash -c "enroot import -o $SQUASH_FILE docker://$IMAGE"
+    if ! srun --jobid=$JOB_ID bash -c "unsquashfs -l $SQUASH_FILE > /dev/null"; then
         echo "unsquashfs failed, removing $SQUASH_FILE and re-importing..."
-        srun --jobid=$JOB_ID bash -c "sudo rm -f $SQUASH_FILE"
-        srun --jobid=$JOB_ID bash -c "sudo enroot import -o $SQUASH_FILE docker://$IMAGE"
+        srun --jobid=$JOB_ID bash -c "rm -f $SQUASH_FILE"
+        srun --jobid=$JOB_ID bash -c "enroot import -o $SQUASH_FILE docker://$IMAGE"
     fi
     srun --jobid=$JOB_ID \
         --container-image=$SQUASH_FILE \
