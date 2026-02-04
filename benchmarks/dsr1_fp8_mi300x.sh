d90f270cacc51ddfd02fc29cab0cc68c3833839d
Chen, Todd <zhenchen@amd.com>
2026-02-04 09:28:09 +0000

    Remove kv-cache-dtype option from DSR1 FP8 MI300X benchmark script.

--- benchmarks/dsr1_fp8_mi300x.sh
+++ benchmarks/dsr1_fp8_mi300x.sh
@@ -45,7 +45,6 @@ python3 -m sglang.launch_server \
 --chunked-prefill-size=131072 \
 --num-continuous-decode-steps=4 \
 --max-prefill-tokens=131072 \
---kv-cache-dtype fp8_e4m3 \
 --attention-backend aiter \
 --disable-radix-cache > $SERVER_LOG 2>&1 &
 
