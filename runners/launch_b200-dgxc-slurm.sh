a408870470dfde1266a5dc0d4d9327f8292502a1
Elnifio <elnifio0519@gmail.com>
2026-02-06 16:29:34 -0800

    revert fp4 support

--- runners/launch_b200-dgxc-slurm.sh
+++ runners/launch_b200-dgxc-slurm.sh
@@ -74,7 +74,6 @@ srtctl_root: "${GITHUB_WORKSPACE}/${SRT_REPO_DIR}"
 # Model path aliases
 model_paths:
   "${MODEL_PREFIX}": "${MODEL_PATH}"
-  "dsr1-fp4": "${MODEL_PATH}"
 # Container aliases
 containers:
   dynamo-trtllm: "${SQUASH_FILE}"
@@ -90,7 +89,7 @@ echo "Running make setup..."
 make setup ARCH=x86_64
 
 echo "Submitting job with srtctl..."
-SRTCTL_OUTPUT=$(srtctl apply -f "$CONFIG_FILE" --tags "b200,dsr1,${PRECISION},${ISL}x${OSL},infmax-$(date +%Y%m%d)" 2>&1)
+SRTCTL_OUTPUT=$(srtctl apply -f "$CONFIG_FILE" --tags "b200,dsr1,fp8,${ISL}x${OSL},infmax-$(date +%Y%m%d)" 2>&1)
 echo "$SRTCTL_OUTPUT"
 
 # Extract JOB_ID from srtctl output
