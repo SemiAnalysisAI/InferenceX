bd4ec30ec4d83fefc403828b61db0fe599c00aab
Cam Quilici <cjquilici@gmail.com>
2026-04-02 10:04:42 -0500

    add exclusive

--- runners/launch_h100-dgxc-slurm.sh
+++ runners/launch_h100-dgxc-slurm.sh
@@ -242,6 +242,7 @@ else
     fi
 
     srun --jobid=$JOB_ID \
+        --exclusive \
         --container-image=$SQUASH_FILE \
         --container-mounts=$GITHUB_WORKSPACE:/workspace/,$HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
         --no-container-mount-home \
