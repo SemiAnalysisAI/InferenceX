8beecaf04d9d7b77f4c0f2f338509d7d6eda0892
hshrivastava-droid <hshrivastava@nvidia.com>
2026-04-28 17:42:36 -0700

    update tp config

--- benchmarks/single_node/qwen3.5_fp8_b200.sh
+++ benchmarks/single_node/qwen3.5_fp8_b200.sh
@@ -48,7 +48,7 @@ PYTHONNOUSERSITE=1 python3 -m sglang.launch_server --model-path=$MODEL --host=0.
 --chunked-prefill-size 16384 \
 --mem-fraction-static 0.8 \
 --stream-interval 50 \
---scheduler-recv-interval $( [[ $CONC -gt 16 ]] && echo 30 || echo 10 ) \
+--scheduler-recv-interval $( [[ $CONC -gt 4 ]] && echo 30 || echo 10 ) \
 --tokenizer-worker-num 6 \
 --context-length $CONTEXT_LENGTH > $SERVER_LOG 2>&1 &
 
