6d498be165b7a4ed48e34b1a83941c96d5c437d0
Oseltamivir <58582368+Oseltamivir@users.noreply.github.com>
2026-05-05 21:12:38 -0700

    Bump DSv4 ATOM warmup reduce fix

--- benchmarks/single_node/dsv4_fp4_mi355x_atom.sh
+++ benchmarks/single_node/dsv4_fp4_mi355x_atom.sh
@@ -112,7 +112,7 @@ fi
 if [ "${ATOM_DSV4_PR650:-1}" = "1" ]; then
     ATOM_PR650_REPO=${ATOM_PR650_REPO:-https://github.com/Oseltamivir/ATOM.git}
     ATOM_PR650_REF=${ATOM_PR650_REF:-dsv4-deep-l0-diag}
-    ATOM_PR650_SHA=${ATOM_PR650_SHA:-ad408dded16cbe134eed4e16c4b84d3eb1fee3fd}
+    ATOM_PR650_SHA=${ATOM_PR650_SHA:-2a5271dc30496e3a7d976c79eba9f4b2a4c05bd5}
     ATOM_PR650_DIR=${ATOM_PR650_DIR:-/tmp/atom-dsv4-pr650}
 
     rm -rf "$ATOM_PR650_DIR"
