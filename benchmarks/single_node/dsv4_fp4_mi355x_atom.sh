dca9aa30b1777cbdf72fb091750d197d32882605
Oseltamivir <58582368+Oseltamivir@users.noreply.github.com>
2026-05-07 20:03:10 -0700

    Bump DSv4 ATOM diagnostics

--- benchmarks/single_node/dsv4_fp4_mi355x_atom.sh
+++ benchmarks/single_node/dsv4_fp4_mi355x_atom.sh
@@ -112,7 +112,7 @@ fi
 if [ "${ATOM_DSV4_PR650:-1}" = "1" ]; then
     ATOM_PR650_REPO=${ATOM_PR650_REPO:-https://github.com/Oseltamivir/ATOM.git}
     ATOM_PR650_REF=${ATOM_PR650_REF:-dsv4-deep-l0-diag}
-    ATOM_PR650_SHA=${ATOM_PR650_SHA:-3734e798b45f9eebb733ef2ea004f7a4afb98010}
+    ATOM_PR650_SHA=${ATOM_PR650_SHA:-96959a2737f30b788632ff299eb34d6c843f9a78}
     ATOM_PR650_DIR=${ATOM_PR650_DIR:-/tmp/atom-dsv4-pr650}
 
     rm -rf "$ATOM_PR650_DIR"
