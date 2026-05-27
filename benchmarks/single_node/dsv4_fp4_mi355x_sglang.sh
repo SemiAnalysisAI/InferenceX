fbddaa30d0fa5405779e55839a39081b926d20cd
thomawan <thomawan@amd.com>
2026-05-27 10:12:59 +0800

    Update config

--- benchmarks/single_node/dsv4_fp4_mi355x_sglang.sh
+++ benchmarks/single_node/dsv4_fp4_mi355x_sglang.sh
@@ -116,6 +116,8 @@ python3 -m sglang.launch_server \
     --disable-radix-cache \
     --attention-backend compressed \
     --max-running-requests ${CONC} \
+    --mem-fraction-static 0.90 \
+    --swa-full-tokens-ratio 0.1 \
     --page-size 256 \
     --context-length $MAX_MODEL_LEN \
     --chunked-prefill-size 8192 \
