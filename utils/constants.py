33bd88872ef09fe9bf776f7dc120bdd66b46c703
Cam Quilici <cjquilici@gmail.com>
2026-07-01 12:00:24 -0500

    chore(agentic): split agentic configs

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,8 @@
-MASTER_CONFIGS = [".github/configs/amd-master.yaml",
-                  ".github/configs/nvidia-master.yaml"]
+MASTER_CONFIGS = [
+    ".github/configs/amd-master.yaml",
+    ".github/configs/amd-agentic.yaml",
+    ".github/configs/nvidia-master.yaml",
+    ".github/configs/nvidia-agentic.yaml",
+]
 RUNNER_CONFIG = ".github/configs/runners.yaml"
 GENERATE_SWEEPS_PY_SCRIPT = "utils/matrix_logic/generate_sweep_configs.py"
