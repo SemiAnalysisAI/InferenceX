4f3cec73b81483929320b27b6781822a2e468e36
Wenyao Gao <wgao11@u.rochester.edu>
2026-07-28 17:01:17 -0700

    test: allow full K3 model startup on MI300X
    Signed-off-by: Wenyao Gao <wgao11@u.rochester.edu>

--- utils/matrix_logic/test_kimik3_mi300x_native.py
+++ utils/matrix_logic/test_kimik3_mi300x_native.py
@@ -75,7 +75,7 @@ def test_kimik3_matrix_is_exactly_four_tp8_pp2_aggregate_jobs() -> None:
         "NATIVE_MULTINODE=1",
         "KIMIK3_NODELIST=chi-mi300x-054,chi-mi300x-043",
         "AITER_SITUV2_A8W4=0",
-        "KIMIK3_STARTUP_TIMEOUT_SECONDS=300",
+        "KIMIK3_STARTUP_TIMEOUT_SECONDS=7200",
         "NCCL_DEBUG=INFO",
         "NCCL_DEBUG_SUBSYS=INIT,NET",
         "NCCL_IB_DISABLE=1",
