a2cb4b337c4f93b5b1b590ef275f6e26c4300620
Sirra <asirra@amd.com>
2026-09-02 11:28:29 +0530

    [AMD] [AGENTX] Kimi Perf Tuning
    Signed-off-by: Sirra <asirra@amd.com>

--- benchmarks/single_node/agentic/kimik3_fp4_mi355x_mtp.sh
+++ benchmarks/single_node/agentic/kimik3_fp4_mi355x_mtp.sh
@@ -82,7 +82,7 @@ if [ "$CONC" -le 4 ]; then
 else
     MAX_NUM_SEQS=$(( CONC + CONC / 4 ))
     if [ "$MAX_NUM_SEQS" -gt 80 ]; then MAX_NUM_SEQS=80; fi
-    MAX_BATCHED_TOKENS=16384
+    MAX_BATCHED_TOKENS=8192
     DCP_SIZE=8
 fi
 export DCP_SIZE
