794cc81f9ab73cf6826f468c68781b17cf74b067
Cam Quilici <cjquilici@gmail.com>
2026-09-23 15:57:22 -0500

    refactor: move MI300X firmware check from preamble to setup hook

--- runners/launch_mi300x-amd.sh
+++ runners/launch_mi300x-amd.sh
@@ -20,7 +20,7 @@ if [[ "$EXECUTION_PATH" == native-single-node ]]; then
     export SALLOC_TIME_LIMIT=180
     export SRT_SRUN_OPTIONS='{"container-remap-root":"", "container-writable":""}'
     SRT_SQUASH_FILE="/raid/inferencex/squash/$(printf '%s' "$IMAGE" | sed 's/[\/:@#]/_/g').sqsh"
-    launch_srt_single_node mi300x-amd
+    launch_srt_single_node mi300x-amd --var GITHUB_WORKSPACE "$GITHUB_WORKSPACE"
     exit $?
 fi
 
